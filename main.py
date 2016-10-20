d7b4e911b560f60537a8d8a734ce3db794419bb7
Daniel194 <lungu.daniel94@gmail.com>
2016-10-20 21:08:22 +0300

    Record 92% .

--- main.py
+++ main.py
@@ -16,5 +16,29 @@ x = tf.placeholder(tf.float32, [None, 784])  # [None, 784]
 # Calculate the labels
 y = tf.nn.softmax(tf.matmul(x, W) + b)
 
+# Cross entropy
+y_ = tf.placeholder(tf.float32, [None, 10])
 
-print(y)
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+
+# Training
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+init = tf.initialize_all_variables()
+
+sess = tf.Session()
+sess.run(init)
+
+for i in range(1000):
+    if i % 10 == 0:
+        print("Step ", i)
+
+    batch_xs, batch_ys = mnist.train.next_batch(100)
+    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+# Evaluate the output
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
