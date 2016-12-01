72c65eab3c7ac34036a12651c40dc01815d650d7
Daniel194 <lungu.daniel94@gmail.com>
2016-12-01 12:13:32 +0200

    CNN2 - Work in Progress

--- CNN2.py
+++ CNN2.py
@@ -15,9 +15,9 @@ class ConvolutionNeuralNetwork(object):
         self.D = d
         self.K = k
         self.NR_VALIDATION_DATA = 50
-        self.NR_ITERATION = 20000
-        self.BATCH_SIZE = 500
-        self.SHOW_ACC = 100
+        self.NR_ITERATION = 20
+        self.BATCH_SIZE = 50
+        self.SHOW_ACC = 1
 
         # Hyperparameter
         self.TRAIN_STEP = 1e-4
@@ -25,31 +25,32 @@ class ConvolutionNeuralNetwork(object):
         self.BETA = 1e-3
 
         # Shape
-        self.W1_1_SHAPE = [3, 3, 3, 32]
-        self.W1_2_SHAPE = [3, 3, 32, 32]
-        self.W2_1_SHAPE = [3, 3, 32, 64]
-        self.W2_2_SHAPE = [3, 3, 64, 64]
-        self.W3_1_SHAPE = [3, 3, 64, 128]
-        self.W3_2_SHAPE = [3, 3, 128, 128]
-        self.WFC_1_SHAPE = [2048, 1024]
-        self.WFC_2_SHAPE = [1024, k]
+        self.W1_1_SHAPE = [3, 3, 1, 128]
+        self.W1_2_SHAPE = [3, 3, 128, 128]
+        self.W2_1_SHAPE = [3, 3, 128, 256]
+        self.W2_2_SHAPE = [3, 3, 256, 256]
 
-        self.B1_SHAPE = [32]
-        self.B2_SHAPE = [64]
-        self.B3_SHAPE = [128]
-        self.BFC1_SHAPE = [1024]
-        self.BFC2_SHAPE = [k]
+        self.WFC_1_SHAPE = [12544, 4096]
+        self.WFC_2_SHAPE = [4096, 2048]
+        self.WFC_3_SHAPE = [2048, k]
+
+        self.B1_SHAPE = [128]
+        self.B2_SHAPE = [256]
+
+        self.BFC1_SHAPE = [4096]
+        self.BFC2_SHAPE = [2048]
+        self.BFC3_SHAPE = [k]
 
     def training(self, features, labels):
         """
         Training the Convolutional Neural Network
-        :param features: the training data [50000 x 3072]
+        :param features: the training data [50000 x 784]
         :param labels: the true label for X  [50000 x 1]
         :return: return a dictionary which contains all learned parameters
         """
 
         # Preprocessing the data
-        features = self.__preprocessing(features)  # [50000 x 3072]
+        features = self.__preprocessing(features)  # [50000 x 784]
 
         # Split data into training and validation sets.
         train_features = features[self.NR_VALIDATION_DATA:]
@@ -62,108 +63,100 @@ class ConvolutionNeuralNetwork(object):
 
         # Placeholders
         x = tf.placeholder(tf.float32, shape=[None, self.D])  # the data
-        y_ = tf.placeholder(tf.int64, shape=[None])  # the true labels
+        y_ = tf.placeholder(tf.float32, shape=[None, self.K])  # the true labels
 
         # Reshape
-        x_image = tf.reshape(x, [-1, 32, 32, 3])
+        x_image = tf.reshape(x, [-1, 28, 28, 1])
 
         # Initialize the weights and the biases
         # First Layer
-        W1_1 = self.__weight_variable(self.W1_1_SHAPE)  # [ 3 x 3 x 3 x 32 ]
-        scale1_1 = tf.Variable(tf.ones(self.B1_SHAPE))  # [32]
-        beta1_1 = tf.Variable(tf.zeros(self.B1_SHAPE))  # [32]
+        W1_1 = self.__weight_variable(self.W1_1_SHAPE)  # [ 3 x 3 x 1 x 128 ]
+        scale1_1 = tf.Variable(tf.ones(self.B1_SHAPE))  # [128]
+        beta1_1 = tf.Variable(tf.zeros(self.B1_SHAPE))  # [128]
 
-        W1_2 = self.__weight_variable(self.W1_2_SHAPE)  # [ 3 x 3 x 32 x 32 ]
-        scale1_2 = tf.Variable(tf.ones(self.B1_SHAPE))  # [32]
-        beta1_2 = tf.Variable(tf.zeros(self.B1_SHAPE))  # [32]
+        W1_2 = self.__weight_variable(self.W1_2_SHAPE)  # [ 3 x 3 x 128 x 128 ]
+        scale1_2 = tf.Variable(tf.ones(self.B1_SHAPE))  # [128]
+        beta1_2 = tf.Variable(tf.zeros(self.B1_SHAPE))  # [128]
 
         # Second Layer
-        W2_1 = self.__weight_variable(self.W2_1_SHAPE)  # [ 3 x 3 x 32 x 64 ]
-        scale2_1 = tf.Variable(tf.ones(self.B2_SHAPE))  # [64]
-        beta2_1 = tf.Variable(tf.zeros(self.B2_SHAPE))  # [64]
+        W2_1 = self.__weight_variable(self.W2_1_SHAPE)  # [ 3 x 3 x 128 x 256 ]
+        scale2_1 = tf.Variable(tf.ones(self.B2_SHAPE))  # [256]
+        beta2_1 = tf.Variable(tf.zeros(self.B2_SHAPE))  # [256]
 
-        W2_2 = self.__weight_variable(self.W2_2_SHAPE)  # [ 3 x 3 x 64 x 64 ]
-        scale2_2 = tf.Variable(tf.ones(self.B2_SHAPE))  # [64]
-        beta2_2 = tf.Variable(tf.zeros(self.B2_SHAPE))  # [64]
-
-        # Third Layer
-        W3_1 = self.__weight_variable(self.W3_1_SHAPE)  # [ 3 x 3 x 64 x 128 ]
-        scale3_1 = tf.Variable(tf.ones(self.B3_SHAPE))  # [64]
-        beta3_1 = tf.Variable(tf.zeros(self.B3_SHAPE))  # [64]
-
-        W3_2 = self.__weight_variable(self.W3_2_SHAPE)  # [ 3 x 3 x 128 x 128 ]
-        scale3_2 = tf.Variable(tf.ones(self.B3_SHAPE))  # [128]
-        beta3_2 = tf.Variable(tf.zeros(self.B3_SHAPE))  # [128]
+        W2_2 = self.__weight_variable(self.W2_2_SHAPE)  # [ 3 x 3 x 256 x 256 ]
+        scale2_2 = tf.Variable(tf.ones(self.B2_SHAPE))  # [256]
+        beta2_2 = tf.Variable(tf.zeros(self.B2_SHAPE))  # [256]
 
         # Full Connected Layer 1
-        WFC1 = self.__weight_variable(self.WFC_1_SHAPE)  # [ 2048 x 1024 ]
-        scaleFC1 = tf.Variable(tf.ones(self.BFC1_SHAPE))  # [1024]
-        betaFC1 = tf.Variable(tf.zeros(self.BFC1_SHAPE))  # [1024]
+        WFC1 = self.__weight_variable(self.WFC_1_SHAPE)  # [ 12544 x 4096 ]
+        scaleFC1 = tf.Variable(tf.ones(self.BFC1_SHAPE))  # [4096]
+        betaFC1 = tf.Variable(tf.zeros(self.BFC1_SHAPE))  # [4096]
 
         # Full Connected Layer 2
-        WFC2 = self.__weight_variable(self.WFC_2_SHAPE)  # [ 1024 x 10 ]
-        bFC2 = self.__bias_variable(self.BFC2_SHAPE)  # [10]
+        WFC2 = self.__weight_variable(self.WFC_2_SHAPE)  # [ 4096 x 2048 ]
+        scaleFC2 = tf.Variable(tf.ones(self.BFC2_SHAPE))  # [2048]
+        betaFC2 = tf.Variable(tf.zeros(self.BFC2_SHAPE))  # [2048]
+
+        # Full Connected Layer 3
+        WFC3 = self.__weight_variable(self.WFC_3_SHAPE)  # [ 2048 x 10 ]
+        bFC3 = self.__bias_variable(self.BFC3_SHAPE)  # [10]
 
         # First Layer
         Z1_1 = self.__convolution(x_image, W1_1)
         batch_mean1_1, batch_var1_1 = tf.nn.moments(Z1_1, [0])
         BN1 = tf.nn.batch_normalization(Z1_1, batch_mean1_1, batch_var1_1, beta1_1, scale1_1, self.EPSILON)
-        H1 = self.__activation(BN1)  # [50000 x 32 x 32 x 32]
+        H1 = self.__activation(BN1)  # [50000 x 28 x 28 x 128]
 
         Z1_2 = self.__convolution(H1, W1_2)
         batch_mean1_2, batch_var1_2 = tf.nn.moments(Z1_2, [0])
         BN1_2 = tf.nn.batch_normalization(Z1_2, batch_mean1_2, batch_var1_2, beta1_2, scale1_2, self.EPSILON)
-        H1_2 = self.__activation(BN1_2)  # [50000 x 32 x 32 x 32]
+        H1_2 = self.__activation(BN1_2)  # [50000 x 28 x 28 x 128]
 
-        H_pool1 = self.__pool(H1_2)  # [50000 x 16 x 16 x 32]
+        H_pool1 = self.__pool(H1_2)  # [50000 x 14 x 14 x 128]
 
         # Second Layer
         Z2_1 = self.__convolution(H_pool1, W2_1)
         batch_mean2_1, batch_var2_1 = tf.nn.moments(Z2_1, [0])
         BN2 = tf.nn.batch_normalization(Z2_1, batch_mean2_1, batch_var2_1, beta2_1, scale2_1, self.EPSILON)
-        H2 = self.__activation(BN2)  # [50000 x 16 x 16 x 64]
+        H2 = self.__activation(BN2)  # [50000 x 14 x 14 x 256]
 
         Z2_2 = self.__convolution(H2, W2_2)
         batch_mean2_2, batch_var2_2 = tf.nn.moments(Z2_2, [0])
         BN2_2 = tf.nn.batch_normalization(Z2_2, batch_mean2_2, batch_var2_2, beta2_2, scale2_2, self.EPSILON)
-        H2_2 = self.__activation(BN2_2)  # [50000 x 16 x 16 x 64]
+        H2_2 = self.__activation(BN2_2)  # [50000 x 14 x 14 x 256]
 
-        H_pool2 = self.__pool(H2_2)  # [50000 x 8 x 8 x 64]
-
-        # Third Layer
-        Z3_1 = self.__convolution(H_pool2, W3_1)
-        batch_mean3_1, batch_var3_1 = tf.nn.moments(Z3_1, [0])
-        BN3 = tf.nn.batch_normalization(Z3_1, batch_mean3_1, batch_var3_1, beta3_1, scale3_1, self.EPSILON)
-        H3 = self.__activation(BN3)  # [50000 x 16 x 16 x 128]
-
-        Z3_2 = self.__convolution(H3, W3_2)
-        batch_mean3_2, batch_var3_2 = tf.nn.moments(Z3_2, [0])
-        BN3_2 = tf.nn.batch_normalization(Z3_2, batch_mean3_2, batch_var3_2, beta3_2, scale3_2, self.EPSILON)
-        H3_2 = self.__activation(BN3_2)  # [50000 x 16 x 16 x 128]
-        H_pool3 = self.__pool(H3_2)  # [50000 x 4 x 4 x 128]
+        H_pool2 = self.__pool(H2_2)  # [50000 x 7 x 7 x 256]
 
         # First Full Connected Layer
-        H_pool3_flat = tf.reshape(H_pool3, [-1, self.WFC_1_SHAPE[0]])  # [ 50000 x 2048 ]
+        H_pool3_flat = tf.reshape(H_pool2, [-1, self.WFC_1_SHAPE[0]])  # [ 50000 x 12544 ]
 
         Z_FC1 = tf.matmul(H_pool3_flat, WFC1)
         batch_mean_fc1, batch_var_fc1 = tf.nn.moments(Z_FC1, [0])
         BN_FC1 = tf.nn.batch_normalization(Z_FC1, batch_mean_fc1, batch_var_fc1, betaFC1, scaleFC1, self.EPSILON)
-        H_fc1 = self.__activation(BN_FC1)  # [ 50000 x 1024 ]
+        H_fc1 = self.__activation(BN_FC1)  # [ 50000 x 4096 ]
 
         # Dropout
         keep_prob = tf.placeholder(tf.float32)
-        H_fc1_drop = tf.nn.dropout(H_fc1, keep_prob)
+        H_fc1_drop = tf.nn.dropout(H_fc1, keep_prob)  # [ 50000 x 4096 ]
 
         # Second Full Connected Layer
-        H_fc2 = tf.matmul(H_fc1_drop, WFC2) + bFC2  # [ 50000 x 10]
+        Z_FC2 = tf.matmul(H_fc1_drop, WFC2)
+        batch_mean_fc2, batch_var_fc2 = tf.nn.moments(Z_FC2, [0])
+        BN_FC2 = tf.nn.batch_normalization(Z_FC2, batch_mean_fc2, batch_var_fc2, betaFC2, scaleFC2, self.EPSILON)
+        H_fc2 = self.__activation(BN_FC2)  # [ 50000 x 2048 ]
 
-        y_conv = tf.nn.softmax(H_fc2)
+        # Dropout
+        H_fc2_drop = tf.nn.dropout(H_fc2, keep_prob)  # [ 50000 x 2048 ]
+
+        # Third Full Connected Layer
+        H_fc3 = tf.matmul(H_fc2_drop, WFC3) + bFC3  # [ 50000 x 10 ]
+
+        y_conv = tf.nn.softmax(H_fc3)
 
         # Loss Function loss fn == avg(y'*log(y))
         loss_fn = -tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]) + self.BETA * tf.nn.l2_loss(
             W1_1) + self.BETA * tf.nn.l2_loss(W1_2) + self.BETA * tf.nn.l2_loss(W2_1) + self.BETA * tf.nn.l2_loss(
-            W2_2) + self.BETA * tf.nn.l2_loss(W3_1) + self.BETA * tf.nn.l2_loss(W3_2) + self.BETA * tf.nn.l2_loss(
-            WFC1) + self.BETA * tf.nn.l2_loss(WFC2)
+            W2_2) + self.BETA * tf.nn.l2_loss(WFC1) + self.BETA * tf.nn.l2_loss(WFC2) + self.BETA * tf.nn.l2_loss(WFC3)
 
         loss_fn_mean = tf.reduce_mean(loss_fn)
 
@@ -203,20 +196,16 @@ class ConvolutionNeuralNetwork(object):
         beta2_2_final = beta2_2.eval()
         scale2_2_final = scale2_2.eval()
 
-        W3_1_final = W3_1.eval()
-        beta3_1_final = beta3_1.eval()
-        scale3_1_final = scale3_1.eval()
-
-        W3_2_final = W3_2.eval()
-        beta3_2_final = beta3_2.eval()
-        scale3_2_final = scale3_2.eval()
-
         WFC1_final = WFC1.eval()
         betaFC1_final = betaFC1.eval()
         scaleFC1_final = scaleFC1.eval()
 
         WFC2_final = WFC2.eval()
-        bFC2_final = bFC2.eval()
+        betaFC2_final = betaFC2.eval()
+        scaleFC2_final = scaleFC2.eval()
+
+        WFC3_final = WFC3.eval()
+        bFC3_final = bFC3.eval()
 
         # Close the session
         sess.close()
@@ -234,17 +223,14 @@ class ConvolutionNeuralNetwork(object):
             'W2_2': W2_2_final,
             'beta2_2': beta2_2_final,
             'scale2_2': scale2_2_final,
-            'W3_1': W3_1_final,
-            'beta3_1': beta3_1_final,
-            'scale3_1': scale3_1_final,
-            'W3_2': W3_2_final,
-            'beta3_2': beta3_2_final,
-            'scale3_2': scale3_2_final,
             'WFC1': WFC1_final,
             'betaFC1': betaFC1_final,
             'scaleFC1': scaleFC1_final,
             'WFC2': WFC2_final,
-            'bFC2': bFC2_final
+            'betaFC2': betaFC2_final,
+            'scaleFC2': scaleFC2_final,
+            'WFC3': WFC3_final,
+            'bFC3': bFC3_final
         }
 
     def predict(self, test_features, nn):
@@ -277,75 +263,65 @@ class ConvolutionNeuralNetwork(object):
         beta2_2 = tf.placeholder(tf.float32, shape=self.B2_SHAPE)  # the beta
         scale2_2 = tf.placeholder(tf.float32, shape=self.B2_SHAPE)  # the scale
 
-        W3_1 = tf.placeholder(tf.float32, shape=self.W3_1_SHAPE)  # the weights
-        beta3_1 = tf.placeholder(tf.float32, shape=self.B3_SHAPE)  # the beta
-        scale3_1 = tf.placeholder(tf.float32, shape=self.B3_SHAPE)  # the scale
-
-        W3_2 = tf.placeholder(tf.float32, shape=self.W3_2_SHAPE)  # the weights
-        beta3_2 = tf.placeholder(tf.float32, shape=self.B3_SHAPE)  # the beta
-        scale3_2 = tf.placeholder(tf.float32, shape=self.B3_SHAPE)  # the scale
-
         WFC1 = tf.placeholder(tf.float32, shape=self.WFC_1_SHAPE)  # the weights
         betaFC1 = tf.placeholder(tf.float32, shape=self.BFC1_SHAPE)  # the beta
         scaleFC1 = tf.placeholder(tf.float32, shape=self.BFC1_SHAPE)  # the scale
 
         WFC2 = tf.placeholder(tf.float32, shape=self.WFC_2_SHAPE)  # the weights
-        bFC2 = tf.placeholder(tf.float32, shape=self.BFC2_SHAPE)  # the biases
+        betaFC2 = tf.placeholder(tf.float32, shape=self.BFC2_SHAPE)  # the beta
+        scaleFC2 = tf.placeholder(tf.float32, shape=self.BFC2_SHAPE)  # the scale
+
+        WFC3 = tf.placeholder(tf.float32, shape=self.WFC_3_SHAPE)  # the weights
+        bFC3 = tf.placeholder(tf.float32, shape=self.BFC3_SHAPE)  # the biases
 
         # Reshape
-        x_image = tf.reshape(x, [-1, 32, 32, 3])
+        x_image = tf.reshape(x, [-1, 28, 28, 1])
 
         # First Layer
         Z1_1 = self.__convolution(x_image, W1_1)
         batch_mean1_1, batch_var1_1 = tf.nn.moments(Z1_1, [0])
         BN1 = tf.nn.batch_normalization(Z1_1, batch_mean1_1, batch_var1_1, beta1_1, scale1_1, self.EPSILON)
-        H1 = self.__activation(BN1)  # [50000 x 32 x 32 x 32]
+        H1 = self.__activation(BN1)  # [50000 x 28 x 28 x 128]
 
         Z1_2 = self.__convolution(H1, W1_2)
         batch_mean1_2, batch_var1_2 = tf.nn.moments(Z1_2, [0])
         BN1_2 = tf.nn.batch_normalization(Z1_2, batch_mean1_2, batch_var1_2, beta1_2, scale1_2, self.EPSILON)
-        H1_2 = self.__activation(BN1_2)  # [50000 x 32 x 32 x 32]
+        H1_2 = self.__activation(BN1_2)  # [50000 x 28 x 28 x 128]
 
-        H_pool1 = self.__pool(H1_2)  # [50000 x 16 x 16 x 32]
+        H_pool1 = self.__pool(H1_2)  # [50000 x 14 x 14 x 128]
 
         # Second Layer
         Z2_1 = self.__convolution(H_pool1, W2_1)
         batch_mean2_1, batch_var2_1 = tf.nn.moments(Z2_1, [0])
         BN2 = tf.nn.batch_normalization(Z2_1, batch_mean2_1, batch_var2_1, beta2_1, scale2_1, self.EPSILON)
-        H2 = self.__activation(BN2)  # [50000 x 16 x 16 x 64]
+        H2 = self.__activation(BN2)  # [50000 x 14 x 14 x 256]
 
         Z2_2 = self.__convolution(H2, W2_2)
         batch_mean2_2, batch_var2_2 = tf.nn.moments(Z2_2, [0])
         BN2_2 = tf.nn.batch_normalization(Z2_2, batch_mean2_2, batch_var2_2, beta2_2, scale2_2, self.EPSILON)
-        H2_2 = self.__activation(BN2_2)  # [50000 x 16 x 16 x 64]
+        H2_2 = self.__activation(BN2_2)  # [50000 x 14 x 14 x 256]
 
-        H_pool2 = self.__pool(H2_2)  # [50000 x 8 x 8 x 64]
-
-        # Third Layer
-        Z3_1 = self.__convolution(H_pool2, W3_1)
-        batch_mean3_1, batch_var3_1 = tf.nn.moments(Z3_1, [0])
-        BN3 = tf.nn.batch_normalization(Z3_1, batch_mean3_1, batch_var3_1, beta3_1, scale3_1, self.EPSILON)
-        H3 = self.__activation(BN3)  # [50000 x 16 x 16 x 128]
-
-        Z3_2 = self.__convolution(H3, W3_2)
-        batch_mean3_2, batch_var3_2 = tf.nn.moments(Z3_2, [0])
-        BN3_2 = tf.nn.batch_normalization(Z3_2, batch_mean3_2, batch_var3_2, beta3_2, scale3_2, self.EPSILON)
-        H3_2 = self.__activation(BN3_2)  # [50000 x 16 x 16 x 128]
-        H_pool3 = self.__pool(H3_2)  # [50000 x 4 x 4 x 128]
+        H_pool2 = self.__pool(H2_2)  # [50000 x 7 x 7 x 256]
 
         # First Full Connected Layer
-        H_pool3_flat = tf.reshape(H_pool3, [-1, self.WFC_1_SHAPE[0]])  # [ 50000 x 2048 ]
+        H_pool3_flat = tf.reshape(H_pool2, [-1, self.WFC_1_SHAPE[0]])  # [ 50000 x 12544 ]
 
         Z_FC1 = tf.matmul(H_pool3_flat, WFC1)
         batch_mean_fc1, batch_var_fc1 = tf.nn.moments(Z_FC1, [0])
         BN_FC1 = tf.nn.batch_normalization(Z_FC1, batch_mean_fc1, batch_var_fc1, betaFC1, scaleFC1, self.EPSILON)
-        H_fc1 = self.__activation(BN_FC1)  # [ 50000 x 1024 ]
+        H_fc1 = self.__activation(BN_FC1)  # [ 50000 x 4096 ]
 
         # Second Full Connected Layer
-        H_fc2 = tf.matmul(H_fc1, WFC2) + bFC2  # [ 50000 x 10]
+        Z_FC2 = tf.matmul(H_fc1, WFC2)
+        batch_mean_fc2, batch_var_fc2 = tf.nn.moments(Z_FC2, [0])
+        BN_FC2 = tf.nn.batch_normalization(Z_FC2, batch_mean_fc2, batch_var_fc2, betaFC2, scaleFC2, self.EPSILON)
+        H_fc2 = self.__activation(BN_FC2)  # [ 50000 x 2048 ]
+
+        # Third Full Connected Layer
+        H_fc3 = tf.matmul(H_fc2, WFC3) + bFC3  # [ 50000 x 10]
 
         # Calculate the output
-        y = tf.nn.softmax(H_fc2)  # [ 50000 x 10]
+        y = tf.nn.softmax(H_fc3)  # [ 50000 x 10]
 
         # Launch the session
         sess = tf.InteractiveSession()
@@ -365,17 +341,14 @@ class ConvolutionNeuralNetwork(object):
             W2_2: nn['W2_2'],
             beta2_2: nn['beta2_2'],
             scale2_2: nn['scale2_2'],
-            W3_1: nn['W3_1'],
-            beta3_1: nn['beta3_1'],
-            scale3_1: nn['scale3_1'],
-            W3_2: nn['W3_2'],
-            beta3_2: nn['beta3_2'],
-            scale3_2: nn['scale3_2'],
             WFC1: nn['WFC1'],
             betaFC1: nn['betaFC1'],
             scaleFC1: nn['scaleFC1'],
             WFC2: nn['WFC2'],
-            bFC2: nn['bFC2']
+            betaFC2: nn['betaFC2'],
+            scaleFC2: nn['scaleFC2'],
+            WFC3: nn['WFC3'],
+            bFC3: nn['bFC3']
         }
 
         # Run model on test data
@@ -460,7 +433,7 @@ class ConvolutionNeuralNetwork(object):
 if __name__ == "__main__":
     # Variable
     learn_data = 'RESULT_data/cifar_10'
-    batch_size = 500
+    batch_size = 50
     features = util.read_features_from_csv('MNIST_data/train.csv')
     labels = util.read_labels_from_csv('MNIST_data/train.csv')
     test_features = util.read_features_from_csv('MNIST_data/test.csv', usecols=None)
@@ -476,7 +449,7 @@ if __name__ == "__main__":
 
         util.pickle_nn(learn_data, nn_parameter)
 
-    util.create_file('RESULT_data/submission_cnn.csv')
+    util.create_file('RESULT_data/submission_cnn2.csv')
 
     for i in range(0, test_features.shape[0], batch_size):
         batch_test_feature = test_features[i:i + batch_size, :]
@@ -484,4 +457,4 @@ if __name__ == "__main__":
         predicted_labels = cnn.predict(batch_test_feature, nn_parameter)
 
         # Save the predictions to label
-        util.append_data_to_file('RESULT_data/submission_cnn.csv', predicted_labels, i)
+        util.append_data_to_file('RESULT_data/submission_cnn2.csv', predicted_labels, i)
