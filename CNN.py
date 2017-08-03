e6e636e76de7a77a4de902b396e39a0e231b30db
Daniel194 <lungu.daniel94@gmail.com>
2017-08-03 23:35:49 +0300

    CNN - new version

--- CNN.py
+++ CNN.py
@@ -7,86 +7,115 @@ from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.utils import np_utils
 from keras import backend as k
+from keras.models import load_model
 
 k.set_image_dim_ordering('th')
 matplotlib.use('Agg')
 
-epochs = 25
-train_size = 42000
-test_size = 2000
-batch_size = 128
-num_classes = 10
 
-img_rows = 28
-img_cols = 28
+class MNIST(object):
+    def __init__(self):
+        self.epochs = 25
+        self.train_size = 42000
+        self.test_size = 2000
+        self.batch_size = 128
+        self.num_classes = 10
 
-data = pd.read_csv("input/train.csv", nrows=42000)
-(train, test) = (data[:train_size], data[42000 - test_size:42000])
+        self.img_rows = 28
+        self.img_cols = 28
 
-x_train = train.ix[:, 1:].values.astype('float32')
-y_train = train.ix[:, 0].values.astype('int32')
+        self.model = Sequential()
 
-x_test = test.ix[:, 1:].values.astype('float32')
-y_test = test.ix[:, 0].values.astype('int32')
+    def load_model(self):
+        self.model = load_model('checkpoint/model.h5')
 
-x_test_raw = x_test
+    def predict(self, images, load_mode=False):
+        if load_mode:
+            self.load_model()
 
-x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-input_shape = (1, img_rows, img_cols)
-y_test_nv = y_test
+        return self.model.predict_classes(images, verbose=0)
 
-x_train /= 255
-x_test /= 255
+    def train(self):
+        self.model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adadelta(),
+                           metrics=['accuracy'])
 
-y_train = np_utils.to_categorical(y_train, num_classes)
-y_test = np_utils.to_categorical(y_test, num_classes)
+        self.model.fit(self.x_train, self.y_train, batch_size=self.batch_size, epochs=self.epochs,
+                       validation_data=(self.x_test, self.y_test))
 
-model = Sequential()
-model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same', input_shape=input_shape))
-model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+        score = self.model.evaluate(self.x_test, self.y_test, verbose=0)
 
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+        print('Test loss:', score[0])
+        print('Test accuracy:', score[1])
 
-model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
-model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+        print("Baseline Error: %.2f%%" % (100 - score[1] * 100))
 
-model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
-model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
-model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.save('checkpoint/model.h5')
 
-model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
-model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
-model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+    def read_data(self):
+        data = pd.read_csv("input/train.csv", nrows=42000)
+        (train, test) = (data[:self.train_size], data[42000 - self.test_size:42000])
 
-model.add(Flatten())
-model.add(Dense(2048, activation='relu'))
-model.add(Dropout(0.25))
-model.add(Dense(2048, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(num_classes, activation='softmax'))
+        x_train = train.ix[:, 1:].values.astype('float32')
+        y_train = train.ix[:, 0].values.astype('int32')
 
-model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adadelta(), metrics=['accuracy'])
+        x_test = test.ix[:, 1:].values.astype('float32')
+        y_test = test.ix[:, 0].values.astype('int32')
 
-history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(x_test, y_test))
+        x_train = x_train.reshape(x_train.shape[0], 1, self.img_rows, self.img_cols)
+        x_test = x_test.reshape(x_test.shape[0], 1, self.img_rows, self.img_cols)
+        self.input_shape = (1, self.img_rows, self.img_cols)
 
-score = model.evaluate(x_test, y_test, verbose=0)
+        x_train /= 255
+        x_test /= 255
 
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+        y_train = np_utils.to_categorical(y_train, self.num_classes)
+        y_test = np_utils.to_categorical(y_test, self.num_classes)
 
-print("Baseline Error: %.2f%%" % (100 - score[1] * 100))
+        self.x_train = x_train
+        self.x_test = x_test
 
-model.predict_classes(x_test, verbose=0)
+        self.y_train = y_train
+        self.y_test = y_test
 
-comp = pd.read_csv("input/test.csv")
-x_comp = comp.iloc[:, :].values
-x_comp = x_comp.reshape(x_comp.shape[0], 1, img_rows, img_cols)
+    def build(self):
+        self.model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same', input_shape=self.input_shape))
+        self.model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
 
-pred = model.predict_classes(x_comp, verbose=0)
+        self.model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
-submissions = pd.DataFrame({"ImageId": list(range(1, len(pred) + 1)), "Label": pred})
-submissions.to_csv("output/submission1.csv", index=False, header=True)
+        self.model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+        self.model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+
+        self.model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+        self.model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+        self.model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+        self.model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        self.model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+        self.model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+        self.model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        self.model.add(Flatten())
+        self.model.add(Dense(2048, activation='relu'))
+        self.model.add(Dropout(0.25))
+        self.model.add(Dense(2048, activation='relu'))
+        self.model.add(Dropout(0.5))
+        self.model.add(Dense(self.num_classes, activation='softmax'))
+
+
+if __name__ == '__main__':
+    comp = pd.read_csv("input/test.csv")
+    x_comp = comp.iloc[:, :].values
+    x_comp = x_comp.reshape(x_comp.shape[0], 1, 28, 28)
+
+    model = MNIST()
+
+    pred = model.predict(x_comp)
+
+    submissions = pd.DataFrame({"ImageId": list(range(1, len(pred) + 1)), "Label": pred})
+    submissions.to_csv("output/submission1.csv", index=False, header=True)
