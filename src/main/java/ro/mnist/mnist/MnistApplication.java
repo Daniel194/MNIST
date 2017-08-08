355107d517f586307d42257fdf85944da699d84b
Daniel194 <lungu.daniel94@gmail.com>
2017-08-08 23:00:17 +0300

    Web application

---
+++ src/main/java/ro/mnist/mnist/MnistApplication.java
@@ -0,0 +1,12 @@
+package ro.mnist.mnist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MnistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MnistApplication.class, args);
+	}
+}
