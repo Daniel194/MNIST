3b1b2a09fb1b687c5e960326d0520408a0bc7f42
Daniel194 <lungu.daniel94@gmail.com>
2017-08-14 21:59:08 +0300

    move hero from frontend to backend

---
+++ backend/src/main/java/ro/mnist/configuration/RepositoryConfig.java
@@ -0,0 +1,16 @@
+package ro.mnist.configuration;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+import ro.mnist.entity.Hero;
+
+@Configuration
+public class RepositoryConfig extends RepositoryRestConfigurerAdapter {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(Hero.class);
+    }
+}
+
